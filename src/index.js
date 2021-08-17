9681b1087cb50a6f7eb1f7939ac3bd22e930c12b
Miguel Angel Morales <mmorales21500@live.com>
2021-08-17 14:05:42 -0400

    adding function

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
-console.log("Hello World <3")
+// fetch("http://localhost:3000/posts")
+// .then(r => r.json())
+// .then(data => console.log(data))
+
+// console.log("Hello World <3")
+
+const postForm = document.getElementById("post-form")
+const postInput = document.getElementById("post-input")
+
+postForm.addEventListener("submit", submitPost)
+
+function submitPost(){
+    event.preventDefault()
+    console.log(postInput.value)
+}
