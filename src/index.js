3292cc0629d7141f380627e26a7582b8175a7735
Miguel Angel Morales <mmorales21500@live.com>
2021-08-25 00:09:09 -0400

    making changes

---
+++ src/index.js
@@ -0,0 +1,121 @@
+// function fetchPosts(){
+// fetch("http://localhost:3000/posts")
+//     .then(r => r.json())
+//     .then(data => console.log(data))
+//     .catch(err => console.warn(err))
+// }
+
+// console.log("Hello World <3")
+
+// const postForm = document.getElementById("post-form")
+// const postInput = document.getElementById("post-input")
+// const postList = document.getElementById("post-list")
+// const postURL = 'http://localhost:3000/posts'
+// const commentURL = 'http://localhost:3000/comments'
+
+// function fetchPosts(){
+//     fetch(postURL)
+//     .then(res => res.json())
+//     .then(posts => posts.forEach(data => renderPost(data.data)))
+     
+// }
+
+// postForm.addEventListener("submit", submitPost)
+
+// function submitPost(){
+//     event.preventDefault()
+//     const configObj = {
+//         method: "POST",
+//         headers:{
+//             "Content-type": "application/json",
+//             "Accept": "application/json"
+//         },
+//         body:JSON.stringify({
+//             content: postInput.value
+//         })
+//     }
+//     fetch(postURL, configObj)
+//     .then(res => res.json())
+//     .then(data => renderPost(data.data))
+// }
+
+// function renderPost(post){
+//     console.log(post)
+//     const li = document.createElement('li')
+//     li.dataset.id = post.id
+
+//     const p = document.createElement('p')
+//     p.innerText = post.attributes.content // Input.value
+
+//     const deleteBtn = document.createElement("button")
+//     deleteBtn.innerText = "delete"
+//     deleteBtn.addEventListener("click", deletePost)
+
+//     const commentForm = document.createElement('form')
+//     commentForm.innerHTML += '<input type="text" id="comment-input"><input type="submit">'
+//     commentForm.addEventListener("submit", renderComment)
+
+//     const commentList = document.createElement('ul')
+//     post.attributes.comments.forEach(comment => {
+//         commentli = document.createElement('li')
+//         li.innerText = comment.content
+//         commentList.appendChild(commentli)
+//     })
+
+//     li.append(p, commentForm, commentList, deleteBtn)
+
+//     postList.appendChild(li)
+
+//     postForm.reset()
+// }
+
+// function deletePost(){
+//     const postId = e.target.parentElement.database.id
+//     fetch('${postURL}/${postId}', {
+//         method: "DELETE"
+//     })
+//     e.target.parentElement.remove()
+// }
+
+// function renderComment(e){
+//     e.preventDefault()
+//     const commentInput = e.target.children[0].value
+//     const commentList = e.target.nextElementSibling
+//     const postId = e.target.parentElement.dataset.id
+    
+//     const li = document.createElement('li')
+//     li.dataset.id = postId
+//     li.innerText = commentInput
+//     commentList.appendChild(li)
+
+//     submitComment(commentInput, postId)
+
+//     e.target.reset()
+// }
+
+// function submitComment(comment, postId){
+//     fetch(commentURL, {
+//         method: "POST",
+//         headers: {
+//             "Content-type": "application/json",
+//             "Accept": "application/json"
+//         },
+//         body: JSON.stringify({
+//             content: comment,
+//             post_id: postId
+//         })
+//     })
+// }
+
+// fetchPosts()
+
+
+const postForm = document.getElementById("post-form")
+const postInput = document.getElementById("post-input")
+const postList = document.getElementById("post-list")
+const postURL = `http://localhost:3000/posts`
+const commentURL = `http://localhost:3000/comments`
+
+postForm.addEventListener("submit", Post.submitPost)
+
+Post.fetchPosts()
---
+++ src/post.js
@@ -0,0 +1,105 @@
+class Post {
+
+    static allPosts = []
+
+    constructor(post){
+        this.id = post.id
+        this.content = post.attributes.content
+        this.comments = post.attributes.comments
+        Post.allPosts.push(this)
+    }
+
+    static renderPosts(){
+        for(let post of this.allPosts){
+            post.renderPost()
+        }
+    }
+
+    static fetchPosts(){
+        fetch(postURL)
+        .then(res => res.json())
+        .then(posts => {
+            for(let post of posts){
+                let newList = new Post(post.data)
+            }
+            this.renderPosts()
+        })
+    }
+
+    renderPost(){
+
+
+
+        // const likesCounter = document.querySelector('.likes')
+        // likesCounter.innerText = `${data.likes}likes`
+
+        // const likeButton = document.querySelector('.like-button')
+
+        // likeButton.addEventListener('click', function(event) {
+        //     likesCounter.innerText = incrementLikes(data)
+        // })
+
+        const li = document.createElement('li')
+        li.dataset.id = this.id
+    
+        const p = document.createElement('p')
+        p.innerText = this.content
+    
+        const deleteBtn = document.createElement("button")
+        deleteBtn.innerText = "delete"
+        deleteBtn.addEventListener("click", this.deletePost)
+    
+        const commentForm = document.createElement('form')
+        commentForm.innerHTML += ` Comment Here <input type="text" id="comment-input"><input type="submit">`
+        commentForm.addEventListener("submit", Comment.createComment)
+    
+        const commentList = document.createElement('ul')
+        this.comments.forEach(comment => {
+            let commentObj = new Comment(comment)
+            console.log(commentObj)
+            commentObj.renderComment(commentList)
+        })
+    
+        li.append(p, deleteBtn, commentForm, commentList)
+    
+        postList.appendChild(li)
+        
+        postForm.reset()
+    }
+
+    static submitPost(){
+        event.preventDefault()
+        const configObj = {
+            method: "POST", 
+            headers: {
+                "Content-type": "application/json", 
+                "Accept": "application/json"
+            }, 
+            body: JSON.stringify({
+                content: postInput.value
+            })
+        }
+    
+        fetch(postURL, configObj)
+        .then(res => res.json())
+        .then(data => {
+            let newPost = new Post(data.data)
+            newPost.renderPost()
+        })
+    
+    }
+
+
+
+    deletePost(){
+        const postId = this.parentElement.dataset.id
+    
+        fetch(`${postURL}/${postId}`, {
+            method: "DELETE"
+        })
+    
+        this.parentElement.remove()
+    }
+
+
+}
